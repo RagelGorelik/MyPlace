e80a452b5b24dd83693f4ff66a61485638744c3a
RagelGorelik <rahelborzova@gmail.com>
2018-01-19 20:21:54 +0200

    my note

---
+++ src/components/Editor.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+
+class Editor extends Component {
+
+  render() {
+    return (
+      <div className="editor column column-45">
+          <SimpleMDE 
+          	onChange={this.props.change}
+          	value={this.props.currentNote.body}
+          	options={{
+          		autofocus:true
+          	}}
+          />
+      </div>
+    );
+  }
+}
+
+export default Editor;
